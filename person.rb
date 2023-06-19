e59776d4daf0dd55fdf7172dd7fae86f7eead3e8
kalbek <access2kaleb@gmail.com>
2023-06-19 15:13:03 +0300

    Add a constructor with name, age, and parent_permission as parameter.

--- person.rb
+++ person.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
+# This class represents a person.
 class Person
   attr_accessor :id, :name, :age
+
+  def initialize(age:, name: 'Unknown', parent_permission: true)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
 end
