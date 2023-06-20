cbb95d541a619ce28cdc672b851b35091aa61329
kalbek <access2kaleb@gmail.com>
2023-06-20 19:26:30 +0300

    Correct correct_name method of capitalize decorator to check for number of characters.

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -7,6 +7,6 @@ class CapitalizeDecorator < BaseDecorator
   end
 
   def correct_name
-    @nameable.correct_name.capitalize
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
   end
 end
