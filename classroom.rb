8df571fe7a95e18645d6ebf7a2c8039c36aef8e3
kalbek <access2kaleb@gmail.com>
2023-06-21 13:07:18 +0300

    Create has-many side (a classroom has many student.)

--- classroom.rb
+++ classroom.rb
@@ -1,7 +1,9 @@
 # This class represent Classroom class.
 class Classroom
-    attr_accessor :label
-    def initialize(label)
-        @label = label
-    end
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
 end
