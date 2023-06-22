8b8ec7eb7fc6e4b7d853af46f618478e8bfe1152
kalbek <access2kaleb@gmail.com>
2023-06-22 10:43:20 +0300

    Fix rental and student, to accept correct parametrs.

--- rental.rb
+++ rental.rb
@@ -1,9 +1,10 @@
 class Rental
   attr_accessor :date, :book, :person
 
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
     @book = book
+    @person = person
     book.add_rental(self)
     person.add_rental(self)
   end
