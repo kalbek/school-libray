52a359c56625996fb93acf86fe9919ccc30dfaf3
kalbek <access2kaleb@gmail.com>
2023-06-22 17:04:29 +0300

    Modify teacher constructor parameter signature.

--- teacher.rb
+++ teacher.rb
@@ -2,8 +2,8 @@
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age:, name: 'Unknown', parent_permission: true, specialization: 'Unknown')
-    super(age: age, name: name, parent_permission: parent_permission)
+  def initialize(age, name = 'Unknown', parent_permission: true, specialization = 'Unknown')
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
